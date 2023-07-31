14be43bab3ab15e0afab3b52bed595d263179fa9
Anis <achadlia0@gmail.com>
2023-07-31 20:13:54 +0300

    book store api

--- bookStoreApi/config/db.js
+++ bookStoreApi/config/db.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 async function connectToDb(){
   //connection to DataBase
   //and this is a promise so it return error or success
-  
   // mongoose
   // .connect(process.env.MONGO_URI)
   // .then(() => console.log('Connected to MongoDB...'))
